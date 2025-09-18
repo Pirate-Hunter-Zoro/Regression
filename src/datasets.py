e95356cc17119d1ef0221b8812433b14c8617f09
Mikey Ferguson <mikeyferguson49@icloud.com>
2025-09-18 09:30:37 -0500

    implemented and tested quadratic data creation

--- src/datasets.py
+++ src/datasets.py
@@ -1,10 +1,65 @@
 import numpy as np
 
-def make_synth_reg_linear(n_train: int=100, n_test: int=20, noise: float=0.1, seed: int=42): 
-    pass
+def make_synth_reg_linear(n_train: int=100, n_test: int=20, noise: float=0.1, seed: int=42) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: 
+    """Generate synthetic linear regression data with the specified number of training and testing cases and noise
 
-def make_synth_reg_quadratic(n_train:int =20, n_test: int=20, noise: float=0.1, seed: int=42):
-    pass
+    Args:
+        n_train (int, optional): Number of training instances. Defaults to 100.
+        n_test (int, optional): Number of testing instances. Defaults to 20.
+        noise (float, optional): Noise to keep the y_values from being boring and perfectly linear. Defaults to 0.1.
+        seed (int, optional): Random seed. Defaults to 42.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: X_train, y_train, X_test, y_test
+    """
+    rng = np.random.RandomState(seed=seed)
+    n = n_train + n_test
+    X_all = rng.rand(n) # all between 0 and 1
+    a = 3
+    b = 5
+    no_noise = a*X_all + b
+    y_all = no_noise + noise*rng.randn(n)
+    shuffled_indices = rng.permutation(n)
+    train_indices = shuffled_indices[:n_train]
+    test_indices = shuffled_indices[n_train:]
+    
+    # Now break up the two inputs and outputs
+    X_train = X_all[train_indices]
+    y_train = y_all[train_indices]
+    X_test = X_all[test_indices]
+    y_test = y_all[test_indices]
+    return (X_train, y_train, X_test, y_test)
+
+def make_synth_reg_quadratic(n_train: int=100, n_test: int=20, noise: float=0.1, seed: int=42) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: 
+    """Generate synthetic quadratic regression data with the specified number of training and testing cases and noise
+
+    Args:
+        n_train (int, optional): Number of training instances. Defaults to 100.
+        n_test (int, optional): Number of testing instances. Defaults to 20.
+        noise (float, optional): Noise to keep the y_values from being boring and perfectly linear. Defaults to 0.1.
+        seed (int, optional): Random seed. Defaults to 42.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: X_train, y_train, X_test, y_test
+    """
+    rng = np.random.RandomState(seed=seed)
+    n = n_train + n_test
+    X_all = rng.rand(n) # all between 0 and 1
+    a = 3
+    b = 5
+    c = 2
+    no_noise = a*(X_all)**2 + b*(X_all) + c
+    y_all = no_noise + noise*rng.randn(n)
+    shuffled_indices = rng.permutation(n)
+    train_indices = shuffled_indices[:n_train]
+    test_indices = shuffled_indices[n_train:]
+    
+    # Now break up the two inputs and outputs
+    X_train = X_all[train_indices]
+    y_train = y_all[train_indices]
+    X_test = X_all[test_indices]
+    y_test = y_all[test_indices]
+    return (X_train, y_train, X_test, y_test)
 
 def load_california(n_train: int=100, n_test: int=20, seed: int=42):
     pass
---
+++ test/test_datasets.py
@@ -0,0 +1,43 @@
+from src.datasets import *
+from src.cv import *
+from src.models_linear import *
+import unittest
+import numpy as np
+
+class TestDatasets(unittest.TestCase):
+    
+    def test_make_synth_reg_linear(self):   
+        n_train = 20
+        n_test = 80
+        X_train, y_train, X_test, y_test = make_synth_reg_linear(n_train, n_test)
+        self.assertTrue(X_train.shape == (n_train,))
+        self.assertTrue(y_train.shape == (n_train,))
+        self.assertTrue(X_test.shape == (n_test,))
+        self.assertTrue(y_test.shape == (n_test,))
+        new_X_train, new_y_train, new_X_test, new_y_test = make_synth_reg_linear(n_train, n_test)
+        self.assertTrue(np.array_equal(X_train, new_X_train) and np.array_equal(X_test, new_X_test) and np.array_equal(y_train, new_y_train) and np.array_equal(y_test, new_y_test))
+        
+    def test_make_synth_reg_quadratic(self):
+        n_train = 20
+        n_test = 80
+        X_train, y_train, X_test, y_test = make_synth_reg_quadratic(n_train, n_test)
+        self.assertTrue(X_train.shape == (n_train,))
+        self.assertTrue(y_train.shape == (n_train,))
+        self.assertTrue(X_test.shape == (n_test,))
+        self.assertTrue(y_test.shape == (n_test,))
+        new_X_train, new_y_train, new_X_test, new_y_test = make_synth_reg_quadratic(n_train, n_test)
+        self.assertTrue(np.array_equal(X_train, new_X_train) and np.array_equal(X_test, new_X_test) and np.array_equal(y_train, new_y_train) and np.array_equal(y_test, new_y_test))
+        # Also test to make sure the data is quadratic
+        
+        _, best, _ = cv_grid_search(model_fn=fit_gd_linear,predict_fn=predict_linear, X=X_train, y=y_train)
+        best_mse = best["mean_metric"]
+        
+        # Now square X_train, and see if we can get a better fit out of that (we should be able to)
+        X_train_squared = np.zeros((n_train, 2))
+        X_train_squared[:,0] = X_train**2 # Find the a for ax
+        X_train_squared[:,1] = X_train # Find the b for bx
+        # We just linearized the problem
+        _, new_best, _ = cv_grid_search(model_fn=fit_gd_linear, predict_fn=predict_linear, X=X_train_squared, y=y_train)
+        new_best_mse = new_best["mean_metric"]
+        
+        self.assertTrue(abs(new_best_mse) < abs(best_mse))
