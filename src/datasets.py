56edac08691f37d51b0ebe9409bcc6331a218bef
Mikey Ferguson <mikeyferguson49@icloud.com>
2025-09-18 10:12:26 -0500

    implemented and tested  making synthetic data for classification

--- src/datasets.py
+++ src/datasets.py
@@ -61,10 +61,47 @@ def make_synth_reg_quadratic(n_train: int=100, n_test: int=20, noise: float=0.1,
     y_test = y_all[test_indices]
     return (X_train, y_train, X_test, y_test)
 
-def load_california(n_train: int=100, n_test: int=20, seed: int=42):
-    pass
+def make_synth_clf(n_train: int=100, n_test: int=20, noise: float=0.1, seed: int=42) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: 
+    """Generate synthetic linear classification data with the specified number of training and testing cases and noise
 
-def make_synth_clf_linear(n_train: int=100, n_test: int=20, noise: float=0.1, seed: int=42):
+    Args:
+        n_train (int, optional): Number of training instances. Defaults to 100.
+        n_test (int, optional): Number of testing instances. Defaults to 20.
+        noise (float, optional): Noise to keep the y_values from being boring and perfectly linear. Defaults to 0.1.
+        seed (int, optional): Random seed. Defaults to 42.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]: X_train, y_train, X_test, y_test
+    """
+    rng = np.random.RandomState(seed=seed)
+    n = n_train + n_test
+    center = 1
+    left_mean = np.array([-center,0])
+    right_mean = np.array([center,0])
+    # "Isotropic covariance" - circular regions, so features are not correlated
+    covariance = noise*noise*np.eye(2) # for BOTH clusters
+    class_1_count = n // 2
+    class_2_count = n - class_1_count
+    
+    # Sample from both classes with their respective multivariate normal distributions
+    class_1_points = rng.multivariate_normal(left_mean,covariance,size=class_1_count)
+    class_2_points = rng.multivariate_normal(right_mean,covariance,size=class_2_count)
+    
+    X = np.concatenate([class_1_points, class_2_points], axis=0) # Shove the two datasets together
+    y = np.ones(n, dtype=int)
+    y[:class_1_count] = 0
+    
+    permuted_indices = rng.permutation(n)
+    X_permuted = X[permuted_indices]
+    y_permuted = y[permuted_indices]
+    
+    train_X = X_permuted[:n_train]
+    test_X = X_permuted[n_train:]
+    train_y = y_permuted[:n_train]
+    test_y = y_permuted[n_train:]
+    return train_X, train_y, test_X, test_y
+
+def load_california(n_train: int=100, n_test: int=20, seed: int=42):
     pass
 
 def load_breast_cancer(n_train: int=100, n_test: int=20, seed: int=42):
--- test/test_datasets.py
+++ test/test_datasets.py
@@ -1,6 +1,7 @@
 from src.datasets import *
 from src.cv import *
 from src.models_linear import *
+from src.models_logistic import *
 import unittest
 import numpy as np
 
@@ -41,3 +42,19 @@ class TestDatasets(unittest.TestCase):
         new_best_mse = new_best["mean_metric"]
         
         self.assertTrue(abs(new_best_mse) < abs(best_mse))
+        
+    def test_make_synth_clf(self):
+        n_train = 20
+        n_test = 80
+        X_train, y_train, X_test, y_test = make_synth_clf(n_train, n_test)
+        self.assertTrue(X_train.shape == (n_train,2))
+        self.assertTrue(y_train.shape == (n_train,))
+        self.assertTrue(X_test.shape == (n_test,2))
+        self.assertTrue(y_test.shape == (n_test,))
+        new_X_train, new_y_train, new_X_test, new_y_test = make_synth_clf(n_train, n_test)
+        self.assertTrue(np.array_equal(X_train, new_X_train) and np.array_equal(X_test, new_X_test) and np.array_equal(y_train, new_y_train) and np.array_equal(y_test, new_y_test))
+        
+        # Test to make sure we did well with classification
+        _, best, _ = cv_grid_search(model_fn=fit_gd_logistic,predict_fn=predict_labels_softmax, X=X_train, y=y_train, param_grid={"iters":[300]}, task="clf")
+        best_accuracy = best["mean_metric"]
+        self.assertTrue(best_accuracy > 0.9)
